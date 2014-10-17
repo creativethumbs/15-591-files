8917c3cc43d25addcfba094cdeaab58042cae40a
creativethumbs <ticha.seth@gmail.com>
2014-10-17 17:49:05 -0400

    realized I was doing things in the wrong way again, changed my methodology to something that makes sense

--- eigenvector-sparql.py
+++ eigenvector-sparql.py
@@ -11,44 +11,70 @@ import string
 
 inputFile = open('edgefile.txt', 'r')
 
-vector_q = SPARQLWrapper('http://localhost:8080/openrdf-workbench/repositories/vector/query')
-vector_u = SPARQLWrapper('http://localhost:8080/openrdf-workbench/repositories/vector/update')
-edges_q = SPARQLWrapper('http://localhost:8080/openrdf-workbench/repositories/edges/query')
-edges_u = SPARQLWrapper('http://localhost:8080/openrdf-workbench/repositories/edges/update')
+sparql_q = SPARQLWrapper('http://localhost:8080/openrdf-workbench/repositories/eigen/query')
+sparql_u = SPARQLWrapper('http://localhost:8080/openrdf-workbench/repositories/eigen/update')
 # for populating the graph (to be used only once)
 def populate():
     for line in inputFile: 
         record = string.split(line, " ")
         src = record[0].rstrip()
         dst = record[1].rstrip()
-        vector_u.setMethod(POST)
-        edges_u.setMethod(POST)
+        sparql_u.setMethod(POST) 
 
-        edges_u.setQuery('PREFIX vert:<http://mygraph.org/vertex/> \
+        sparql_u.setQuery('PREFIX vert:<http://mygraph.org/vertex/> \
                          PREFIX link:<http://mygraph.org/linkedto> \
                          INSERT DATA {vert:'+src+' link: vert:'+dst+'}')
-        edges_u.query()
+        sparql_u.query()
 
-        vector_u.setQuery('PREFIX vert:<http://mygraph.org/vertex/> \
+        sparql_u.setQuery('PREFIX vert:<http://mygraph.org/vertex/> \
                          PREFIX score:<http://mygraph.org/score> \
-                         INSERT DATA {vert:'+src+' score: \"0.001\"}')
-        vector_u.query()
+                         INSERT DATA {vert:'+src+' score: 0.001}')
+        sparql_u.query()
 
-        
 #populate() 
-#def update():
 
-def query():
-    sparql.setMethod(GET)
-    sparql.setQuery('SELECT ?s ?p ?o {?s ?p ?o} ORDER BY ?s ?p')
+def MVM():
+    del_array = [] # array for storing values to delete
+    ins_array = [] # array for storing values to insert
 
-    results = sparql.query().convert()
+    sparql_q.setMethod(GET)
+    sparql_q.setQuery('PREFIX score:<http://mygraph.org/score> \
+                        SELECT ?s ?p ?o \
+                        {?s ?p ?o . \
+                         FILTER(?p = score:)} ')
+    prev_scores = sparql_q.query().convert()
+    s = prev_scores.toxml()
+    obj = xmltodict.parse(s)
+    json_input = json.dumps(obj) #this is a string
+    decoded = json.loads(json_input) #this is a json object(?)
+    print json.dumps(obj,sort_keys=True,indent=4)
 
+    # populates the array with the vector elements we need to delete later
+    for i in range (len(decoded['sparql']['results']['result'])):
+        nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']['#text']
+        edge = decoded['sparql']['results']['result'][i]['binding'][1]['uri']
+        score = decoded['sparql']['results']['result'][i]['binding'][2]['literal']['#text']
+        elem = '<' + nid + '> <' + edge + '> ' + score
+        temp_array.append(elem)
+
+    print temp_array[0]
+
+'''
+    sparql_q.setQuery('PREFIX link:<http://mygraph.org/linkedto> \
+                        PREFIX score:<http://mygraph.org/score> \
+                        SELECT ?src (SUM(?score) AS ?totalScore) \
+                        {?nid score: ?score . \
+                        ?src link: ?dst . \
+                        FILTER(?dst = ?nid) \
+                        } GROUP BY ?src')
+
+    results = sparql_q.query().convert()
+    vectorSum = 0
     s = results.toxml()
     obj = xmltodict.parse(s)
     json_input = json.dumps(obj) #this is a string
     decoded = json.loads(json_input) #this is a json object(?)
-    #print json.dumps(obj,sort_keys=True,indent=4)
+    print json.dumps(obj,sort_keys=True,indent=4)
 
     for i in range (len(decoded['sparql']['results']['result'])):
         print decoded['sparql']['results']['result'][i]['binding'][0]['uri']['#text']
@@ -59,4 +85,5 @@ def query():
             print decoded['sparql']['results']['result'][i]['binding'][2]['uri']['#text']
         else:
             print decoded['sparql']['results']['result'][i]['binding'][2]['literal']
-
+'''
+MVM()
