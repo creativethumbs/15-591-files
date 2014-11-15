c6c3fb5a4eee0de1049c1d7ddb902aadccdb1259
creativethumbs <ticha.seth@gmail.com>
2014-11-15 00:45:55 -0500

    I think I fixed some weird errors

--- calcCP-sherlock.py
+++ calcCP-sherlock.py
@@ -1,4 +1,4 @@
-#You MUST start the Sherlock database before running the program!
+#You MUST start the Sherlock database before running the programnot 
 
 import requests
 
@@ -27,6 +27,9 @@ s.post('https://sherlock.psc.edu/urika/gam',data=userinfo)
 headers = {'content-type': 'application/x-www-form-urlencoded'}
 
 def CP(iterNo): 
+    updatea = False
+    updateb = False
+    updatec = False
     #all scores from previous iteration
     #allscores = 'PREFIX ascore:<http://vector-a-score'+str(iterNo-1)+'> \
     #PREFIX bscore:<http://vector-b-score'+str(iterNo-1)+'> \
@@ -38,8 +41,8 @@ def CP(iterNo):
     # check com2 pg.6
     sumofscores_a = 'PREFIX hasdest:<http://has_dest> \
                         PREFIX onport:<http://on_port> \
-                        PREFIX bscore:<http://vector-b-score'+str(iterNo-1)+'> \
-                        PREFIX cscore:<http://vector-c-score'+str(iterNo-1)+'> \
+                        PREFIX bscore:<http://vector-b-score> \
+                        PREFIX cscore:<http://vector-c-score> \
                         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
                         SELECT ?srcip (SUM(xsd:double(?bscore) * xsd:double(?cscore)) AS ?totalScore) \
                         {?vector_b bscore: ?bscore . \
@@ -51,8 +54,8 @@ def CP(iterNo):
 
     sumofscores_b = 'PREFIX hasdest:<http://has_dest> \
                         PREFIX onport:<http://on_port> \
-                        PREFIX ascore:<http://vector-a-score'+str(iterNo-1)+'> \
-                        PREFIX cscore:<http://vector-c-score'+str(iterNo-1)+'> \
+                        PREFIX ascore:<http://vector-a-score> \
+                        PREFIX cscore:<http://vector-c-score> \
                         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
                         SELECT ?srcip (SUM(xsd:double(?ascore) * xsd:double(?cscore)) AS ?totalScore) \
                         {?vector_a ascore: ?ascore . \
@@ -64,8 +67,8 @@ def CP(iterNo):
 
     sumofscores_c = 'PREFIX hasdest:<http://has_dest> \
                         PREFIX onport:<http://on_port> \
-                        PREFIX ascore:<http://vector-a-score'+str(iterNo-1)+'> \
-                        PREFIX bscore:<http://vector-b-score'+str(iterNo-1)+'> \
+                        PREFIX ascore:<http://vector-a-score> \
+                        PREFIX bscore:<http://vector-b-score> \
                         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
                         SELECT ?srcip (SUM(xsd:double(?ascore) * xsd:double(?bscore)) AS ?totalScore) \
                         {?vector_a ascore: ?ascore . \
@@ -75,23 +78,16 @@ def CP(iterNo):
                         FILTER(?dstip = ?vector_a && ?dstip2 = ?vector_b) \
                         } GROUP BY ?srcip'
 
-    #mass deletion
-    deletescores = 'PREFIX ascore:<http://vector-a-score'+str(iterNo-1)+'> \
-                    PREFIX bscore:<http://vector-b-score'+str(iterNo-1)+'> \
-                    PREFIX cscore:<http://vector-c-score'+str(iterNo-1)+'> \
-                    DELETE {?s ?p ?o} WHERE {?s ?p ?o . \
-                    FILTER (?p = ascore: || ?p = bscore: || ?p = cscore:)}' 
-
     #uses scores from previous calculation
-    sumofsquares_a = 'PREFIX score:<http://vector-a-score'+str(iterNo)+'> \
+    sumofsquares_a = 'PREFIX score:<http://vector-a-score> \
                         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
                         SELECT (SUM(xsd:double(?score) * xsd:double(?score)) as ?vectorSum) \
                         {?s score: ?score}'
-    sumofsquares_b = 'PREFIX score:<http://vector-b-score'+str(iterNo)+'> \
+    sumofsquares_b = 'PREFIX score:<http://vector-b-score> \
                         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
                         SELECT (SUM(xsd:double(?score) * xsd:double(?score)) as ?vectorSum) \
                         {?s score: ?score}'
-    sumofsquares_c = 'PREFIX score:<http://vector-c-score'+str(iterNo)+'> \
+    sumofsquares_c = 'PREFIX score:<http://vector-c-score> \
                         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> \
                         SELECT (SUM(xsd:double(?score) * xsd:double(?score)) as ?vectorSum) \
                         {?s score: ?score}'
@@ -103,20 +99,25 @@ def CP(iterNo):
     # first equation ---------------------------------------------
     qsumofscores_a = {'query': sumofscores_a}
     result = s.get('https://sherlock.psc.edu/dataset/sparql/LBNL/query',params=qsumofscores_a)
-    
+     
     xml_source = result.text
     obj = xmltodict.parse(xml_source)
     json_input = json.dumps(obj)
     decoded = json.loads(json_input) 
 
+    #print json.dumps(obj,sort_keys=True,indent=4)
+    #updatea = len(decoded['sparql']['results']['result']) > 1
     # populates the array with the vector elements we need to insert
     for i in range (len(decoded['sparql']['results']['result'])):
-        nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']
-        score = decoded['sparql']['results']['result'][i]['binding'][1]['literal']['#text']
-        edge = 'http://vector-a-score'+str(iterNo)
-        elem = '<' + nid + '> <' + edge + '> "' + score + '"'
-        
-        ins_array.append(elem)
+        if (i>0):
+            nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']
+            score = decoded['sparql']['results']['result'][i]['binding'][1]['literal']['#text']
+            edge = 'http://vector-a-score'
+            elem = '<' + nid + '> <' + edge + '> "' + score + '"'
+            elem2 = '<' + nid + '> <' + edge + '>'
+            
+            ins_array.append(elem)
+            del_array.append(elem2)
 
     print "first equation passed"
 
@@ -129,14 +130,18 @@ def CP(iterNo):
     json_input = json.dumps(obj)
     decoded = json.loads(json_input) 
 
+    #updateb = len(decoded['sparql']['results']['result']) > 1
     # populates the array with the vector elements we need to insert
     for i in range (len(decoded['sparql']['results']['result'])):
-        nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']
-        score = decoded['sparql']['results']['result'][i]['binding'][1]['literal']['#text']
-        edge = 'http://vector-b-score'+str(iterNo)
-        elem = '<' + nid + '> <' + edge + '> "' + score + '"'
-        
-        ins_array.append(elem)
+        if (i>0):
+            nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']
+            score = decoded['sparql']['results']['result'][i]['binding'][1]['literal']['#text']
+            edge = 'http://vector-b-score'
+            elem = '<' + nid + '> <' + edge + '> "' + score + '"'
+            elem2 = '<' + nid + '> <' + edge + '>'
+            
+            ins_array.append(elem)
+            del_array.append(elem2)
 
     print "second equation passed"
 
@@ -151,21 +156,30 @@ def CP(iterNo):
 
     #print json.dumps(obj,sort_keys=True,indent=4)
 
+    #updatec = len(decoded['sparql']['results']['result']) > 1
     # populates the array with the vector elements we need to insert
     for i in range (len(decoded['sparql']['results']['result'])):
-        nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']
-        score = decoded['sparql']['results']['result'][i]['binding'][1]['literal']['#text']
-        edge = 'http://vector-c-score'+str(iterNo)
-        elem = '<' + nid + '> <' + edge + '> "' + score + '"'
-        
-        ins_array.append(elem)
+        if (i>0):
+            nid = decoded['sparql']['results']['result'][i]['binding'][0]['uri']
+            score = decoded['sparql']['results']['result'][i]['binding'][1]['literal']['#text']
+            edge = 'http://vector-c-score'
+            elem = '<' + nid + '> <' + edge + '> "' + score + '"'
+            elem2 = '<' + nid + '> <' + edge + '>'
+            
+            ins_array.append(elem)
+            del_array.append(elem2)
 
     print "third equation passed"
 
+    print("ins_array has size " + str(len(ins_array)))
     # inserts all calculations into graph ---------------------------------------------------
-    for item in ins_array:
-        updatestring ={'update': 'INSERT DATA {'+item+'}'}
-        s.post('https://sherlock.psc.edu/dataset/sparql/LBNL/update',headers=headers,params=updatestring)
+    for i in range ( len(ins_array)):
+        print ins_array[i]
+        updatestring = 'DELETE {' + del_array[i] + ' ?o } \
+                        INSERT {' + ins_array[i] +'} \
+                        WHERE {' + del_array[i] + ' ?o } '
+        updated = {'update': updatestring}
+        s.post('https://sherlock.psc.edu/dataset/sparql/LBNL/update',headers=headers,params=updated)
 
     #gets the vectorSum for the first vector table -----------------------------------
     qsumofsquares_a = {'query': sumofsquares_a}
@@ -177,7 +191,7 @@ def CP(iterNo):
     decoded = json.loads(json_input) 
 
     sumsquare_a = decoded['sparql']['results']['result']['binding']['literal']['#text']   
-    vectorSum_a = math.sqrt(float(sumsquare))
+    vectorSum_a = math.sqrt(float(sumsquare_a))
 
     #gets the vectorSum for the second vector table -----------------------------------
     qsumofsquares_b = {'query': sumofsquares_b}
@@ -189,7 +203,7 @@ def CP(iterNo):
     decoded = json.loads(json_input) 
 
     sumsquare_b = decoded['sparql']['results']['result']['binding']['literal']['#text']   
-    vectorSum_b = math.sqrt(float(sumsquare))
+    vectorSum_b = math.sqrt(float(sumsquare_b))
 
     #gets the vectorSum for the third vector table -----------------------------------
     qsumofsquares_c = {'query': sumofsquares_c}
@@ -201,33 +215,47 @@ def CP(iterNo):
     decoded = json.loads(json_input) 
 
     sumsquare_c = decoded['sparql']['results']['result']['binding']['literal']['#text']   
-    vectorSum_c = math.sqrt(float(sumsquare))
+    vectorSum_c = math.sqrt(float(sumsquare_c))
 
-    #deletes all scores from the previous calculation so they can be updated------------
-    deletestring ={'update': deletescores}
-    s.post('https://sherlock.psc.edu/dataset/sparql/LBNL/update',headers=headers,params=deletestring)
-
-
+    #mass deletion-----------------------------------------------------------------------
+    #brute force... 
+    
+    #print("ins_array has size " + str(len(ins_array)))----------------------------------
+    print "final loop"
     for item in ins_array:
+        #print (item)
         sourceip = item.split()[0]
         old_score = item.split()[2][1:-1] #need to remove the double quotes
         edgelabel = item.split()[1]
-        if (edgelabel == 'http://vector-a-score'+str(iterNo)):
+        new_score = ""
+
+        delitem = sourceip + " " + edgelabel # + " \"" + old_score + "\""
+ 
+        #deletes all scores from the previous calculation so they can be updated------------
+        #deletestring ={'update': 'DELETE DATA {'+delitem+'}' }
+        #s.post('https://sherlock.psc.edu/dataset/sparql/LBNL/update',headers=headers,params=deletestring)
+
+        if (edgelabel == '<http://vector-a-score>'):
             new_score = str(float(old_score) / float(vectorSum_a))
 
-        elif (edgelabel == 'http://vector-b-score'+str(iterNo)):
+        elif (edgelabel == '<http://vector-b-score>'):
             new_score = str(float(old_score) / float(vectorSum_b))
 
-        elif (edgelabel == 'http://vector-c-score'+str(iterNo)):
+        elif (edgelabel == '<http://vector-c-score>'):
             new_score = str(float(old_score) / float(vectorSum_c))
 
         newitem = sourceip + ' ' + edgelabel + ' "' + new_score + '"'
-        
-        insertstring ={'update': 'INSERT DATA {'+newitem+'}'}
+        print newitem
 
-        status2 = s.post('https://sherlock.psc.edu/dataset/sparql/LBNL/update',headers=headers,params=insertstring)
+        if(new_score != ""):
+            updatestring = 'DELETE {' + delitem + ' ?o } \
+                        INSERT {' + newitem +'} \
+                        WHERE {' + delitem + ' ?o } '
+            #insertstring ={'update': 'INSERT DATA {'+newitem+'}'}
+            updated = {'update': updatestring}
+            s.post('https://sherlock.psc.edu/dataset/sparql/LBNL/update',headers=headers,params=updated)
 
-        output_array.append(vectorname + ', ' + str(iterNo) + ', ' + new_score)
+            output_array.append(sourceip + ', ' + str(iterNo) + ', ' + new_score)
 
 
 def writeToFile():
@@ -236,10 +264,10 @@ def writeToFile():
 
 iterNo = 1
 
-while iterNo < 2 :
+while iterNo < 5 :
     iterNo += 1
     CP(iterNo) 
-    # print "done with iterNo " + str(iterNo)
+    print "done with iterNo " + str(iterNo)
 
 writeToFile()
 
